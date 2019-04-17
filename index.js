79c782b7b04bd0483100e90443e65f68c7a41e48
Pat Cavit <github@patcavit.com>
2019-04-17 10:12:56 -0700

    feat: nicer error when selector isn't found

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const { toMatchSnapshot } = require("jest-snapshot");
 module.exports = (page = global.page) => {
     expect.extend({
         async toMatchDOMSnapshot(selector, ...args) {
-            let el;
-            
+            let el = selector;
+
             // Support both string selectors and ElementHandles
             if(typeof selector === "string") {
                 el = await page.$(selector);
-            } else {
-                el = selector;
+
+                if(!el) {
+                    throw new Error(`Unable to locate element matching: ${selector}`);
+                }
             }
 
             const source = await page.evaluate((element) => element.outerHTML, el);
-            
+
             return toMatchSnapshot.call(
                 this,
                 source,
