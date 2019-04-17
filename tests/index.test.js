0eeb8007ff8b32b3ccef6c070378eb53d1131d62
Pat Cavit <github@patcavit.com>
2019-04-17 10:13:10 -0700

    test: test for new behavior

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,23 +1,29 @@
 "use strict";
 
 describe(".toMatchDOMSnapshot()", () => {
-    it("should accept a string", async () => {
+    beforeEach(async () => {
         await page.goto(require.resolve("./fixtures/one.html"));
-        
+    });
+
+    it("should accept a string", async () => {
         await expect(".one").toMatchDOMSnapshot();
     });
-    
-    it("should accept an ElementHandler", async () => {
-        await page.goto(require.resolve("./fixtures/one.html"));
 
+    it("should accept an ElementHandle", async () => {
         const el = await page.$(".one");
 
         await expect(el).toMatchDOMSnapshot();
     });
-    
-    it("should support named snapshots", async () => {
-        await page.goto(require.resolve("./fixtures/one.html"));
 
+    it("should support named snapshots", async () => {
         await expect(".one").toMatchDOMSnapshot("named");
+    });
+
+    it("should give useful errors when a selector isn't found", async () => {
+        try {
+            await expect(".two").toMatchDOMSnapshot();
+        } catch(e) {
+            expect(e.toString()).toMatchSnapshot();
+        }
     });
 });
